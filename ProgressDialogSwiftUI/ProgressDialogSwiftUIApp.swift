cd2d8d6f622ba3e7e5f837c154afc6a1a44872da
Std1o <37378410+Std1o@users.noreply.github.com>
2021-05-08 20:32:31 +0300

    Initial commit

---
+++ ProgressDialogSwiftUI/ProgressDialogSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressDialogSwiftUIApp.swift
+//  ProgressDialogSwiftUI
+//
+//  Created by Илья on 08.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct ProgressDialogSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
